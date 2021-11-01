7dba484f689460e057e7c65f84595988a891cb34
unknown <YUHAOLEE@hotaimotor.com.tw>
2021-11-01 08:45:25 +0800

    Fix Test Case

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "L Jackson";
+            return "Les Jackson";
         }
 
         // POST api/values
